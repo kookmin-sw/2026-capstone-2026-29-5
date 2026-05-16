27b5aee901bb220c545c3581e908c89cae81e883
aincon <coran4178@gmail.com>
2026-05-17 03:49:15 +0900

    차지 중 대쉬를 쓸 경우 이펙트 사라짐 수정

--- Assets/01_Script/StateMachine/TrailOff.cs
+++ Assets/01_Script/StateMachine/TrailOff.cs
@@ -3,15 +3,30 @@ using UnityEngine;
 public class TrailOff : StateMachineBehaviour
 {
     [Tooltip("비워두면 모든 Trail을 끔. 이름 지정하면 그 이름만 끔.")]
-    public string trailName = "";
+    public string[] trailNames;
 
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         var trails = animator.GetComponentsInChildren<Tiny.Trail>(true);
+        bool turnOffAll = (trailNames == null || trailNames.Length == 0);
+
         foreach (var t in trails)
         {
-            if (string.IsNullOrEmpty(trailName) || t.name == trailName)
+            if (turnOffAll)
+            {
                 t.enabled = false;
+            }
+            else
+            {
+                foreach (var name in trailNames)
+                {
+                    if (t.name == name)
+                    {
+                        t.enabled = false;
+                        break;
+                    }
+                }
+            }
         }
     }
 }
